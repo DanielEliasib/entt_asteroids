13915bae007e328d609535bf71c4d949c218f5e4
Daniel E <daniel.eliasib.al@outlook.com>
2024-03-20 20:09:14 -0600

    Added initial prototypes for a FSM

---
+++ asteroids/include/utils/state.hpp
@@ -0,0 +1,112 @@
+
+#include <cassert>
+#include <functional>
+#include <memory>
+#include <unordered_map>
+
+class state;
+
+class state_transition {
+   public:
+    state_transition(std::function<bool()> condition, std::shared_ptr<state> target_state) :
+        _condition(condition),
+        _target_state(target_state)
+    {
+        assert(_condition != nullptr);
+        assert(_target_state != nullptr);
+    };
+
+    bool evaluate() const { return _condition(); };
+    std::shared_ptr<state> target_state() const { return _target_state; };
+
+   protected:
+    std::function<bool()> _condition     = nullptr;
+    std::shared_ptr<state> _target_state = nullptr;
+};
+
+class state {
+   public:
+    state(std::function<void()> on_enter, std::function<void()> on_exit, std::function<void()> update) :
+        _on_enter(on_enter),
+        _on_exit(on_exit),
+        _update(update){};
+
+    void add_transition(std::function<bool()> condition, std::shared_ptr<state> target_state)
+    {
+        _transitions.push_back(state_transition(condition, target_state));
+    }
+
+   protected:
+    void update()
+    {
+        if (_update != nullptr)
+        {
+            _update();
+        }
+    };
+
+    bool evaluate_transitions(std::shared_ptr<state>& next_state)
+    {
+        for (auto& transition : _transitions)
+        {
+            if (!transition.evaluate())
+                continue;
+
+            on_exit();
+            next_state = transition.target_state();
+            next_state->on_enter();
+            return true;
+        }
+
+        return false;
+    }
+
+    void on_enter()
+    {
+        if (_on_enter != nullptr)
+        {
+            _on_enter();
+        }
+    };
+
+    void on_exit()
+    {
+        if (_on_exit != nullptr)
+        {
+            _on_exit();
+        }
+    };
+
+    std::vector<state_transition> _transitions;
+
+    std::function<void()> _on_enter = nullptr;
+    std::function<void()> _on_exit  = nullptr;
+    std::function<void()> _update   = nullptr;
+
+    friend class state_machine;
+};
+
+class state_machine {
+   public:
+    state_machine(std::shared_ptr<state> initial_state) :
+        _current_state(initial_state) { assert(_current_state != nullptr); };
+
+    void update()
+    {
+        std::shared_ptr<state> next_state = nullptr;
+
+        if (_current_state != nullptr)
+        {
+            _current_state->update();
+            _current_state->evaluate_transitions(next_state);
+        }
+
+        if (next_state != nullptr)
+        {
+            _current_state = next_state;
+        }
+    }
+
+   protected:
+    std::shared_ptr<state> _current_state = nullptr;
+};
--- asteroids/main.cpp
+++ asteroids/main.cpp
@@ -6,6 +6,7 @@
 #include <components/player.hpp>
 #include <entt/entt.hpp>
 #include <math.hpp>
+#include <memory>
 #include <processors/physics_processors.hpp>
 #include <processors/render_processors.hpp>
 
@@ -14,8 +15,53 @@
 #include "components/render.hpp"
 #include "processors/base_processors.hpp"
 #include "utils/input_handler.hpp"
+#include "utils/state.hpp"
 
 using entt::operator""_hs;
+
+void state_machine_test()
+{
+    int print_count = 0;
+
+    auto print_enter = [&print_count]() {
+        std::cout << "Enter printing state" << std::endl;
+        print_count = 0;
+    };
+    auto print_update = [&print_count]() {
+        std::cout << "Update printing state" << std::endl;
+        print_count++;
+    };
+    auto print_exit = []() { std::cout << "Exit printing state" << std::endl; };
+
+    auto print_condition = [&print_count]() { return print_count > 3; };
+
+    auto warning_enter = [&print_count]() {
+        std::cout << "Enter WARNING state" << std::endl;
+        print_count = 0;
+    };
+    auto warning_update = [&print_count]() {
+        std::cout << "Update WARNING state" << std::endl;
+        print_count++;
+    };
+    auto warning_exit = []() { std::cout << "Exit WARNING state" << std::endl; };
+
+    auto warning_condition = [&print_count]() { return print_count > 6; };
+
+    std::shared_ptr<state> printing_state = std::make_shared<state>(print_enter, print_exit, print_update);
+    std::shared_ptr<state> warning_state  = std::make_shared<state>(warning_enter, warning_exit, warning_update);
+
+    printing_state->add_transition(print_condition, warning_state);
+    warning_state->add_transition(warning_condition, printing_state);
+
+    auto machine = state_machine(printing_state);
+
+    for (int i = 0; i < 13; i++)
+    {
+        machine.update();
+    }
+
+    std::cout << "End of state machine test" << std::endl;
+}
 
 int main()
 {
@@ -26,6 +72,8 @@ int main()
     Color background_color = {15, 15, 15, 255};
     Color text_color       = {204, 191, 147, 255};
 
+    state_machine_test();
+
     entt::registry registry;
 
     entt::scheduler general_scheduler;
@@ -35,7 +83,7 @@ int main()
     general_scheduler.attach<boundary_process>(registry);
 
     entt::scheduler render_scheduler;
-	render_scheduler.attach<sprite_sequence_process>(registry);
+    render_scheduler.attach<sprite_sequence_process>(registry);
     render_scheduler.attach<sprite_render_process>(registry);
     render_scheduler.attach<shape_render_process>(registry);
     render_scheduler.attach<ui_process>(registry);
